ea256ba580da67c2585ead9c1bf1764d349c3643
Ben Pringle <ben.pringle@gmail.com>
2014-04-11 16:18:00 -0400

    Set up poll creation interface (front-end only)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'VoteAnon' });
+});
+
+router.route('/create')
+.get(function(req, res) {
+  res.render('create', { title: 'VoteAnon: create poll' });
+})
+.post(function(req, res) {
+  res.render('confirm_create', { title: 'VoteAnon: poll created' });
 });
 
 module.exports = router;
